eb6f70b6f40ab309db8f417b9f30df91017e625c
unknown <Rezadi Falah>
2021-04-17 22:41:00 +0700

    fix register

--- src/components/Modal/ModalRegister.js
+++ src/components/Modal/ModalRegister.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import ReactDom from 'react-dom'
+import ModalLogin from '../Modal/ModalLogin'
+import './ModalRegister.css'
 
-function ModalRegister() {
-    return (
-        <div>
+function ModalRegister({opens, onClose}) {
+    const [isOpen, setIsOpen] = useState(false)
+    const [isOpens, setIsOpens] = useState(true)
+
+
+    const switchToLogin = () => {
+        setIsOpen(true)
+    }
+
+    if (isOpen) {
+        // logic here
+    }
+
+    if (!opens) return null
+
+    return ReactDom.createPortal (
+        <>
             <div className='dark-overlay' onClick={onClose}></div>
             <div className='modal-register'>
-                <h5 className="modal-title">Register</h5>
-                <div className='input-group'>
-                    <input type='text' placeholder=' Email' className='input-email'></input>
-                    <input type='password' placeholder=' Password' className='input-password'></input>
+                <div className='modal-sample-content'>
+                    <h1>Register</h1>
+                    <div className='input-group-sample'>
+                        <input type='text' placeholder='Email' className='email-input grab-input'></input>
+                        <input type='password' placeholder='Password' className='password-input grab-input'></input>
+                        <input type='text' placeholder='Full Name' className='name-input grab-input'></input>
+                        
+                    </div>
+                    <div style={{textAlign: 'center'}} className='modal-sample-link'>Register</div>
+                    <p style={{textAlign: 'center'}}>Already have an account ? 
+                        <span onClick={switchToLogin}>
+                            Click Here
+                        </span>
+                    </p>
+                    <ModalLogin open={isOpen} onClose={() => setIsOpen(false)}></ModalLogin>
                 </div>
             </div>
-        </div>
+        </>,
+        document.getElementById('portal')
     )
 }
 
